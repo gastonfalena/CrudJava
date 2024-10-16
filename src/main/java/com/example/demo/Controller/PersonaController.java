f1c8fc4c7900e8137a240b096e09cf081d0115a5
gastonfalena <gastonfalena@gmail.com>
2024-10-16 17:03:35 -0300

    chore: update API endpoints

--- src/main/java/com/example/demo/Controller/PersonaController.java
+++ src/main/java/com/example/demo/Controller/PersonaController.java
@@ -8,23 +8,36 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@RequestMapping("/personas")
 public class PersonaController {
 
     @Autowired
     private IPersonaService persoServ;
 
-    @GetMapping("/personas/traer")
+    @GetMapping("/traer")
     public List<Persona> getPersonas() {
         return persoServ.getPersonas();
     }
-    @PostMapping("personas/crear")
+    @PostMapping("/crear")
     public String savePersona (@RequestBody Persona perso){
         persoServ.savePersona(perso);
         return "La persona fue creada correctamente";
     }
-    @DeleteMapping("personas/borrar/{id}")
+    @DeleteMapping("/borrar/{id}")
     public String deletePersona(@PathVariable Long id){
-        
+        persoServ.deletePersona(id);
+        return "La persona fue eliminada correctamente";
+    }
+    @PutMapping("/editar/{id_original}")
+    public Persona editPersona(@PathVariable Long id_original,
+                               @RequestParam(required = false,name="id") Long nuevaId,
+                               @RequestParam(required = false,name="nombre") String nuevoNombre,
+                               @RequestParam(required = false,name="apellido") String nuevoApellido,
+                               @RequestParam(required = false,name="edad") int nuevaEdad){
+        persoServ.editPersona(id_original, nuevaId, nuevoNombre,nuevoApellido,nuevaEdad);
+        Persona perso = persoServ.findPersona(nuevaId);
+        return perso;
+
     }
 
 
